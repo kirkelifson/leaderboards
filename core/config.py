668550ea95a459c86013a1c251348a1fe02f788d
Gus Naughton <gus@gusnaughton.com>
2015-12-24 12:39:05 -0500

    I have brain damage. :(

--- core/config.py
+++ core/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/leaderboards'
-SECRET_KEY = "\xdf\xcf\x94{-\x8c\x9e{^V'%\xa2m\x8b\xa02Q\xaa\xa1\xe4N\xfb\xc0"
+SQLALCHEMY_DATABASE_URI = ''
+SECRET_KEY = ""
