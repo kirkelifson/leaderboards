046d2fb71cd2e986b42d803f29112bc5920941dd
RabsRincon <rubrinbla@gmail.com>
2016-02-12 23:09:28 +0100

    Dashboard first implementation.. Forgot to commit __init__ last time

--- core/routes/dashboard.py
+++ core/routes/dashboard.py
@@ -1,11 +1,56 @@
 __author__ = 'gnaughton'
-from flask import render_template, redirect, request, flash, url_for
-from flask_login import login_user, logout_user
+from flask import render_template, redirect, request, flash, url_for, abort
+from flask_login import login_user, logout_user, current_user
 
 from core import app
-from core.models import DBUser
+from core.models import DBUser, DBTeam
+from core.routes.defuseraccess import lvl_min_userof_momentumteam
 
+dashboard_destinations = ['home','manage']
 
+def is_valid_destination(destination):
+    return destination in dashboard_destinations
+
+@app.route('/dashboard/<destination>')
+def dashboard(destination = 'home'):
+    if current_user.get_id() is not None and current_user.is_authenticated:
+        if not is_valid_destination(destination):
+            destination = 'home'
+        return render_template('dashboard/index.html', destination=destination)
+    else:
+        flash('You can\'t access here without login in first')
+        return redirect(url_for('login', next='dashboard_r_home'))
+@app.route('/dashboard')
 @app.route('/dashboard/')
-def dashboard():
-    return render_template('dashboard/index.html')
+def dashboard_r_home():
+    return redirect(url_for('dashboard',destination='home'))
+
+@app.route('/dashboard/manage')
+def dashboard_manage():
+    if current_user.get_id() is not None and current_user.is_authenticated and current_user.access >= lvl_min_userof_momentumteam():
+        return render_template('dashboard/manage.html',destination='manage')
+    else:
+        flash('Authorized personnel only.', category='login')
+        return redirect(url_for('login', next='dashboard_r_home'))
+
+@app.route('/dashboard/home')
+def dashboard_home():
+    if current_user.get_id() is not None and current_user.is_authenticated:
+        return render_template('dashboard/home.html',destination='home')
+    else:
+        flash('You need to log in before being able to acces that resource.', category='login')
+        return redirect(url_for('login', next='dashboard_r_home'))
+
+def momteam_pending():
+    if current_user.get_id() is not None and current_user.is_authenticated and current_user.access >= lvl_min_userof_momentumteam():
+        member = DBTeam.query.filter_by(steamid=current_user).first()
+        if member is not None:
+            if member.realname is None or member.role is None:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+app.jinja_env.globals.update(momteam_pending=momteam_pending)
--- core/routes/main.py
+++ core/routes/main.py
@@ -1,10 +1,8 @@
 __author__ = 'gnaughton'
 
-from flask import render_template, redirect, request, flash, url_for
-from flask_login import login_user, logout_user
-
+from flask import render_template, url_for
 from core import app
-from core.models import DBUser
+from core.models import db, DBTeam
 
 import random
 
@@ -12,29 +10,18 @@ import random
 def index():
     return render_template('index.html')
 
+@app.route('/team')
+def team():
+    team = DBTeam.query.order_by(DBTeam.priority).all()
+    return render_template('team.html', team=team)
+
 def url_for_random_background():
     number = random.randint(1, 3)
     return url_for('static', filename='frontend/images/' 'bg-'+ str(number) +'.jpg')
 
 app.jinja_env.globals.update(url_for_random_background=url_for_random_background)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    username = request.form['username']
-    password = request.form['password']
-    dbuser = DBUser.query.filter_by(username=username).first()
+def place_steamlink():
+    return '<a href=\"http://steampowered.com\">Powered by Steam</a>'
 
-    if dbuser is None or not dbuser.check_password(password):
-        flash('Username or password is invalid', 'danger')
-        return redirect(url_for('login'))
-    login_user(dbuser)
-    flash('Logged in successfully', 'success')
-    return redirect(request.args.get('next') or url_for('dashboard'))
-
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
+app.jinja_env.globals.update(place_steamlink=place_steamlink)
