ea3722bf4c56d8890656d3f05e6c020175772b09
RabsRincon <rubrinbla@gmail.com>
2016-02-14 23:05:52 +0100

    team file is now able to display credits.txt

--- core/routes/main.py
+++ core/routes/main.py
@@ -3,6 +3,8 @@ __author__ = 'gnaughton'
 from flask import render_template, url_for
 from core import app
 from core.models import db, DBTeam
+from core.routes.defuseraccess import rank_momentum_senior, access_required
+from flask_login import current_user
 
 import random
 
@@ -13,7 +15,16 @@ def index():
 @app.route('/team')
 def team():
     team = DBTeam.query.order_by(DBTeam.priority).all()
-    return render_template('team.html', team=team)
+    showlink = False
+    if current_user is not None and current_user.is_authenticated and current_user.access >= rank_momentum_senior:
+        showlink = True
+    return render_template('team.html', team=team, asfile=False, showlink=showlink)
+
+@app.route('/team/credits.txt')
+@access_required(rank_momentum_senior)
+def team_credits():
+    team = DBTeam.query.order_by(DBTeam.priority).all()
+    return render_template('team.html', team=team, asfile=True, time=(3 * db.session.query(DBTeam).count()) + 10)
 
 def url_for_random_background():
     number = random.randint(1, 3)
